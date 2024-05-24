cb1443dc03e9597ac0260bd8958c821063668044
irycka <xarkovskaya.est@gmail.com>
2024-05-24 17:14:37 +0300

    Added the search functionality using API

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
+}
+
+function searchCity(city) {
   let apiKey = "at4d96bda05b6f73380b5219of43773b";
-  let apiURL = `https://api.shecodes.io/weather/v1/current?lon=${city}&key=${apiKey}&units=metric`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Kharkiv");
